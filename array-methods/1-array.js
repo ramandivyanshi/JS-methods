1bfda7d9a733c292c16f163027d3498c4eb71d42
Divyanshi Raman <56720908+ramandivyanshi@users.noreply.github.com>
2020-08-25 03:09:21 +0530

    Array methods Ex-1

--- array-methods/1-array.js
+++ array-methods/1-array.js
@@ -49,23 +49,37 @@ function ascending(a, b) {
 }
 numbers.sort(ascending);
 console.log(numbers);
-VM54: 6(11) [1, 3, 4, 5, 6, 7, 9, 11, 12, 18, 101]
-    // Remove the last word in strings
-
+//[1, 3, 4, 5, 6, 7, 9, 11, 12, 18, 101]
+// Remove the last word in strings
+var strings = ["this", "is", "a", "collection", "of", "words"];
+strings.pop();
+console.log(strings);
 // Add a new word in the array
-
+var strings = ["this", "is", "a", "collection", "of", "words"];
+strings.push("and_letters");
+console.log(strings);
 // Remove the first word in the array
-
+strings.splice(0, 1);
+console.log(strings);
 // Place a new word at the start of the array use (upshift)
-
+var strings = ["this", "is", "a", "collection", "of", "words"];
+strings.unshift("Well!");
+console.log(strings);
 // Make a subset of numbers array [18,9,7,11]
-
+var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
+numbers.splice(3, 4);
 // Make a subset of strings array ['a','collection']
-
+var strings = ["this", "is", "a", "collection", "of", "words"];
+strings.splice(2, 2);
 // Replace 12 & 18 with 1221 and 1881
-
+var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
+numbers.splice(1, 1, 1221);
+numbers.splice(3, 1, 1881);
+console.log(numbers);
 // Replace words with string in strings array
-
+var strings = ["this", "is", "a", "collection", "of", "words"];
+strings.splice(5, 1, "string")
+console.log(strings);
 // Customers Array
 var customers = [
     { firstname: "Joe", lastname: "Blogs" },
@@ -74,7 +88,21 @@ var customers = [
     { firstname: "Jack", lastname: "White" }
 ];
 // Find all customers whose firstname starts with 'J'
-
+var findj = customers.filter(elem =>
+    elem.firstname.startsWith('J')
+)
+console.log(findj);
 // Create new array with firstname and lastname
-
+var newArr = ["joe", "Blogs", "John", "Smith", "Dave", "Jones", "Jack", "White"];
 // Sort the array created above alphabetically
+var newArr = ["Joe", "Blogs", "John", "Smith", "Dave", "Jones", "Jack", "White"];
+newArr.sort(function(a, b) {
+    if (a < b) {
+        return -1;
+    }
+    if (b < a) {
+        return 1;
+    }
+    return 0;
+});
+console.log(newArr);
